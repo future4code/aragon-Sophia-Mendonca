0301d8cdeaedd4c1143bf37252cb966567e37a57
Sophia Silva de Mendonça <contatosophiamendonca@gmail.com>
2022-04-01 19:40:56 -0300

    projeto-blackjack

---
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -0,0 +1,29 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+       // Projeto
+
+   console.log("seja bem-vindo ao jogo blackjack")
+
+   confirm("deseja iniciar o jogo, sim ou não?")
+
+   confirm("deseja iniciar o jogo, sim ou não?")
+   if(confirm("deseja iniciar o jogo, sim ou não?")) {
+      iniciaJogo();
+   }
+   else {
+      console.log("obrigado por jogar")
+   }
+   function( iniciaJogo () {for(let i=0; i<4; i++) {
+   }
+   ))
