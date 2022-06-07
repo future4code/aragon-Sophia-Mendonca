6dc50de3e2377d9d1b95beff19d62397952e496a
Sophia Silva de Mendonça <contatosophiamendonca@gmail.com>
2022-06-07 18:58:54 -0300

    dia2, labeddit

--- modulo3/LabEddit/labeddit/src/App.js
+++ modulo3/LabEddit/labeddit/src/App.js
@@ -1,8 +1,11 @@
+import Router from "./routes/Router";
+import GlobalState from "./global/GlobalState";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <GlobalState>
+      <Router />
+    </GlobalState>
   );
 }
 
